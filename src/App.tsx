b0cd3199223812eb2d531de71ab05fc2ecf96e2d
taserbeat <taserbeat@gmail.com>
2022-08-11 13:54:27 +0900

    セクション13-105: CSSを適用してスタイルを調整しよう

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,36 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="scene" ref={divRef}></div>
+      <div className="webgl" ref={divRef}></div>
+
+      <main>
+        <h1>ポートフォリオ</h1>
+
+        <section>
+          <h2>突き詰める最高のスキルを</h2>
+        </section>
+
+        <section>
+          <h2>My Project</h2>
+          <p>私の作った作品です</p>
+        </section>
+
+        <section>
+          <h2>My Skills</h2>
+          <p>HTML/CSS/JavaScript</p>
+        </section>
+
+        <section>
+          <h2>Enginer</h2>
+          <p>学び続ける好奇心</p>
+        </section>
+
+        <section>
+          <h2>Contact</h2>
+          <p>お問合せはこちら</p>
+          <button>こちらから</button>
+        </section>
+      </main>
     </div>
   );
 };
