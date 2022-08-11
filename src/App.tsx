6ca5b012ebe9f2afd6d35319412878c11dfb7b14
taserbeat <taserbeat@gmail.com>
2022-08-11 14:05:01 +0900

    セクション13-108: Three.jsにおける共通実装をコーディングで練習しよう

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,67 @@
 import { useEffect, useRef } from "react";
+import * as THREE from "three";
 
 import "./App.css";
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
+// シーン
+const scene = new THREE.Scene();
+
+// カメラ
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// レンダラー
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 const App = () => {
-  const divRef = useRef<HTMLDivElement>(null);
+  const refDiv = useRef<HTMLDivElement>(null);
+
+  const handleOnBrowserResize = () => {
+    // カメラのアスペクト比を修正
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    // レンダラーを修正
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    return;
+  };
 
   useEffect(() => {
+    refDiv.current?.appendChild(renderer.domElement);
+
+    window.addEventListener("resize", handleOnBrowserResize);
+
     const updateRender = () => {
+      // レンダー
+      renderer.render(scene, camera);
+
+      // アニメーションを更新
       requestAnimationFrame(updateRender);
     };
 
     updateRender();
 
-    return () => {};
+    return () => {
+      refDiv.current?.removeChild(renderer.domElement);
+      window.removeEventListener("resize", handleOnBrowserResize);
+    };
   }, []);
 
   return (
     <div className="App">
-      <div className="webgl" ref={divRef}></div>
+      <div className="webgl" ref={refDiv}></div>
 
       <main>
         <h1>ポートフォリオ</h1>
